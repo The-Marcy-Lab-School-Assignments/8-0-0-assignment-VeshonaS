3600dba807ce1744e27ac6b24071c02285a52ab9
VeshonaS <aniahshell1@gmail.com>
2024-09-11 12:56:12 -0400

    done with bugs

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res,next) =>{
+app.get('/api', (req, res, next) =>{
    const {name, favAnimal} = req.query 
   res.send(`hi ${name} you lke fluffy ${favAnimal}`)
+})
+
+app.get('/api',(req, res, next) => {
+  const {favColor} = req.query  
+  res.send(`your favorite color is ${favColor}`)
+})
+
+app.get('/greetings', (req, res, next) => {
+  res.send('<h1>Hello World</h1>')
+})
+app.get('/farewell', (req, res, next) => {
+  res.send('<h1>Goodbye World</h1>')
 })
 
  const PORT = 8080; 
